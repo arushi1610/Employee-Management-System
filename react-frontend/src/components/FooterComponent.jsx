5c513f02a615a4c93c1f8dddb488802ee5df53b8
Arushi <aruruthala2001@gmail.com>
2023-10-29 19:40:31 +0530

    intial commit

---
+++ react-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class FooterComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <footer className = "footer">
+                    <span className="text-muted">Made by Arushi Ruthala</span>
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default FooterComponent
